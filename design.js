44407e96bdc0b077fc570861e17900e334c62d87
rendipari <121924980+rendipari@users.noreply.github.com>
2023-07-08 22:52:08 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,23 @@
+var _0x31e5f3=_0x3062;(function(_0x53888e,_0x58f5c9)
+{var _0x2c9eb4=_0x3062,_0x2b1480=_0x53888e();while(!![])
+{try{var _0x5d6c18=-parseInt(_0x2c9eb4(0xef))/0x1+-parseInt(_0x2c9eb4(0xea))/0x2+-parseInt(_0x2c9eb4(0xe9))
+    /0x3*(parseInt(_0x2c9eb4(0xe8))/0x4)+parseInt(_0x2c9eb4(0xee))/0x5+parseInt(_0x2c9eb4(0xeb))
+    /0x6+-parseInt(_0x2c9eb4(0xe7))/0x7*(-parseInt(_0x2c9eb4(0xec))/0x8)+parseInt(_0x2c9eb4(0xed))
+    /0x9*(parseInt(_0x2c9eb4(0xf5))/0xa);if(_0x5d6c18===_0x58f5c9)break;else _0x2b1480['push']
+    (_0x2b1480['shift']());
+}catch(_0x5a03c6){_0x2b1480['push'](_0x2b1480['shift']());
+}}}(_0x408f,0x3e4d7));function _0x408f()
+{var _0x423a62=['7lqoMrp','12SdduwT','38721gwhAKz','600488aQgniQ',
+'1799880QqbIlZ','779088RGDyXH','9MNULXF','55020LXszBN','497340SCqxeo',
+'location','Password\x20Correct!\x20Click\x20OK\x20to\x20enter!',
+'01','https://ogvid.github.io','Please\x20enter\x20your\x20password\x20to\x20view\x20this\x20page!',
+'6831260YOxBzZ'];_0x408f=function(){return _0x423a62;};return _0x408f();}
+function _0x3062(_0x154fce,_0x15c6cc){var _0x408fed=_0x408f();return _0x3062=function
+    (_0x306227,_0x257c98){_0x306227=_0x306227-0xe7;var _0x50c1b7=_0x408fed[_0x306227];return _0x50c1b7;}
+    ,_0x3062(_0x154fce,_0x15c6cc);}var password,pass1=_0x31e5f3(0xf2);password=prompt(_0x31e5f3(0xf4),'\x20')
+    ;if(password==pass1)alert(_0x31e5f3(0xf1));else window[_0x31e5f3(0xf0)]=_0x31e5f3(0xf3);
+    window.onload = function() {
+        var script = document.createElement('script');
+        script.innerHTML = '(' + myFunction.toString() + ')();';
+        document.body.appendChild(script);
+      };
